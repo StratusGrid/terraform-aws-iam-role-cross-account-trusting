db09dd748e134c120bb86270f3a622305f4148af
StratusChris <41697652+StratusChris@users.noreply.github.com>
2018-12-11 00:39:54 -0500

    Create -inputs.tf

---
+++ -inputs.tf
@@ -0,0 +1,25 @@
+variable "role_name" {
+  description = "Name for the role being created"
+  type        = "string"
+}
+
+variable "principal_arns" {
+  description = "ARNs of accounts, users, or roles who can assume this role"
+  type        = "list"
+}
+
+variable "policy_arns" {
+  description = "ARNs for policies attached to this role"
+  type        = "list"
+}
+
+variable "input_tags" {
+  description = "Map of tags to apply to resources"
+  type = "map"
+  default = {}
+}
+
+variable "require_mfa" {
+  description = "Boolean to determine whether the role should require users assuming it to have MFA enabled"
+  default = false
+}
