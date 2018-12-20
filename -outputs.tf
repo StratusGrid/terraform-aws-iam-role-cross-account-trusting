52ceb5949c0e0af972f8854b54e98a7a23715b0d
StratusChris <41697652+StratusChris@users.noreply.github.com>
2018-12-20 17:34:56 -0500

    fixes after rebuild

--- -outputs.tf
+++ -outputs.tf
@@ -1,4 +1,4 @@
 output "role_arn" {
   description = "ARN of newly created role"
-  value = "${join(",", concat(aws_iam_role.cross_account_assume_role_mfa.*.arn, aws_iam_role.cross_account_assume_role.*.arn))}"
+  value = "$(aws_iam_role.cross_account_assume_role.arn}"
 }
