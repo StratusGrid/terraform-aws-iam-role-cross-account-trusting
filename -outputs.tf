3aea9ce723c6fafd09552c223bf78927dc613087
Chris Hurst <chris.hurst@stratusgrid.com>
2019-05-31 18:06:43 -0400

    fixed typo in output

--- -outputs.tf
+++ -outputs.tf
@@ -1,4 +1,4 @@
 output "role_arn" {
   description = "ARN of newly created role"
-  value = "$(aws_iam_role.cross_account_assume_role.arn}"
+  value = "${aws_iam_role.cross_account_assume_role.arn}"
 }
