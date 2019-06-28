26e08144fdf8e453c167c0efdcaf37fd2d6dc9cd
Chris Hurst <chris.hurst@stratusgrid.com>
2019-06-28 09:06:52 -0400

    added output

--- -outputs.tf
+++ -outputs.tf
@@ -2,3 +2,8 @@ output "role_arn" {
   description = "ARN of newly created role"
   value = "${aws_iam_role.cross_account_assume_role.arn}"
 }
+
+output "role_name" {
+  description = "Name of newly created role"
+  value = "${aws_iam_role.cross_account_assume_role.name}"
+}
